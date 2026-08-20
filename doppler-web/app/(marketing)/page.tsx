ea2e65660cabe57e0a16d11fe14f85832dc75685
Ignacio Irigoitia <ignacio.irigoitia@gmail.com>
2026-08-20 19:51:56 -0300

    delete unnused import

--- doppler-web/app/(marketing)/page.tsx
+++ doppler-web/app/(marketing)/page.tsx
@@ -2,7 +2,6 @@ import Hero from "@/components/marketing/Hero";
 import ClientLogos from "@/components/marketing/ClientLogos";
 import Services from "@/components/marketing/Services";
 import AiSection from "@/components/marketing/AiSection";
-import Metrics from "@/components/marketing/Metrics";
 import Testimonials from "@/components/marketing/Testimonials";
 import FAQ from "@/components/marketing/FAQ";
 import FinalCTA from "@/components/marketing/FinalCTA";
